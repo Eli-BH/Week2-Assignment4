85f843675a2372aef1a544b3a9071e2f034d509c
Eli-b-good <eli.i.henderson95@gmail.com>
2020-06-01 14:39:20 -0400

    added login form, and pasword event listener

--- Assign3.js
+++ Assign3.js
@@ -12,4 +12,19 @@ const dntHvrH1 = document.getElementById('dntHvrH1');
 dntHvrH1.addEventListener('mouseover', () => {
     alert('Hey, I told you not to hover over me!')
 });
+let truePass = 12345678;
+
+let fPass = document.getElementById('password'); 
+let sub = document.getElementById('submit'); 
+
+
+sub.addEventListener('click', (i) => {
+    const fValuePass = document.getElementById('password').value; 
+    if(fValuePass != truePass){
+        alert("Incorrect Password");
+    } else {
+        i.preventDefault();
+        document.getElementById('loginInfo').innerHTML = "<h1>Welcome!</h1>";
+    }
+});
 
